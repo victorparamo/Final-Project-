65937461c2be6f3ae96a1c0bbab84e5564e95740
victor <victor.h.paramo.24@gmail.com>
2018-11-20 18:33:07 -0600

    Se agrega el login final.

--- src/components/GalleryImage/GalleryImage.js
+++ src/components/GalleryImage/GalleryImage.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { Button, Card, CardBody, CardImage, CardTitle, CardText } from 'mdbreact';
+import { Button, Card, CardBody, CardImage, CardTitle, CardText,
+         Modal, ModalBody, ModalHeader, ModalFooter
+} from 'mdbreact';
 import CartActions from '../../redux/actions/Cart_Actions';
 
-const GalleryImage = (props) =>{
+class GalleryImage extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            modal: false
+        };
+    }
 
-    const { addToCart,
-            removeFromCart,
-            item,
-    } = props;
+    toggle() {
+        this.setState({
+          "modal": !this.state.modal
+        });
+    }
 
-    console.log("-----Item------>", item);
+    render(){
+        const { addToCart,
+                removeFromCart,
+                item,
+        } = this.props;
 
-    return (
+        console.log("-----Item------>", item);
+        return (
             <div className="cardComponent">   
                 <Card>
                     <CardImage
@@ -23,11 +37,22 @@ const GalleryImage = (props) =>{
                     />
                     <CardBody>
                         <CardTitle>{item.name}</CardTitle>
-                        <Button className={item.amount > 0 ? "":"disabled"} href="#">Button</Button>
+                        <Button className={item.amount > 0 ? "":"disabled"} onClick={() => this.toggle()}>Button</Button>
                     </CardBody>
                 </Card>
+                <Modal isOpen={this.state.modal} toggle={() => this.toggle()} centered>
+                    <ModalHeader toggle={() => this.toggle()}>{item.name}</ModalHeader>
+                    <ModalBody>
+                        <img src={item.image} className="img-fluid" alt="Responsive image" /> 
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="secondary" onClick={() => this.toggle()}>Close</Button>
+                        <Button color="primary">Save changes</Button>
+                    </ModalFooter>
+                </Modal>
             </div>
-    );
+        )
+    }
 };
 
 function mapDispatchToProps (dispatch) {
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import logo from './logo.svg';
+import logo from '../../images/logo1.png';
+import loginImage from '../../images/SignIn.jpg';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import FontAwesome from 'react-fontawesome';
@@ -8,27 +9,41 @@ import { GoogleClientID } from '../../keys/Google_Keys';
 import loginActions from '../../redux/actions/Login_Actions';
 import { NavLink } from 'react-router-dom';
 import { Button } from 'mdbreact';
+import './Login.css';
 
 const Login = (props) =>{
-    const { loginAuthorized, loginFailed, fetchInventory } = props;
+    const { loginAuthorized, loginFailed } = props;
 
     return (
          <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <GoogleLogin
-                clientId={GoogleClientID}
-                onSuccess={(e) => loginAuthorized(e.profileObj)}
-                onFailure={(e) => loginFailed(e)}
-            >
-                <FontAwesome
-                    name='google'
-                    size='2x'
-                    className="mr-3"
-                />
-                <a>Login with google</a>
-            </GoogleLogin>
-            <Button className="my-4" color="cyan" outline><NavLink className="text-white" to="/workSpace">Go to page without Login... </NavLink></Button>
-         </div>
+            <div className="Login_Left">   
+                <img src={loginImage} className="Login-Image" alt="logo" />
+            </div>
+            <div className="Login_Right">   
+                <img src={logo} className="App-logo" alt="logo" />
+                <p className="loginName">ART GALLERY STORE</p>
+                <div className="slogan">
+                    <p style={{color:"#00C851"}}>Here art selects you </p>
+                </div>
+                <div className="loginFooter">
+                    <GoogleLogin    
+                        clientId={GoogleClientID}
+                        onSuccess={(e) => loginAuthorized(e.profileObj)}
+                        onFailure={(e) => loginFailed(e)}
+                    >
+                        <FontAwesome
+                            name='google'
+                            size='2x'
+                            className="GoogleIcon mr-3"
+                        />
+                        <a className="googleButtonTitle">Google</a>
+                    </GoogleLogin>
+                </div>
+                <div className="loginFooter2">
+                    <Button className="pageButton mt-2 mr-0" color="success"><NavLink className="text-white" to="/workSpace">Go to page without Login... </NavLink></Button>
+                </div>
+            </div>
+        </div>
     );
 };
   
